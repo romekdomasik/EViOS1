51052f75f528c5a7b240f8291eb55d57fd71d119
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-06-30 09:17:58 +0200

    round image
    round profile pic added

--- EViOS1/EViOS1/ViewController.swift
+++ EViOS1/EViOS1/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profilePic: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profilePic.layer.cornerRadius = profilePic.frame.height / 2
     }
 
 
