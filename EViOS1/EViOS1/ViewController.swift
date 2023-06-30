c3a9a9eda56a6b7ec4d2e0e8411502cc518bae53
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-06-30 10:43:12 +0200

    password field functionnal
    added functions to eye image and visibility of the password field. Also added functionnalities to the keyboard

--- EViOS1/EViOS1/ViewController.swift
+++ EViOS1/EViOS1/ViewController.swift
@@ -13,11 +13,36 @@ class ViewController: UIViewController {
     @IBOutlet var welcomeLabel: UILabel!
     @IBOutlet var loginField: UITextField!
     @IBOutlet var passwordField: UITextField!
+    @IBOutlet var eye: UIImageView!
+    @IBOutlet var labelInscription: UILabel!
+    @IBOutlet var btnCoulissant: UISwitch!
+    @IBOutlet var btnLogin: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        
+        let tapOnView = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapOnView)
+    
         profilePic.layer.cornerRadius = profilePic.frame.height / 2
+        btnCoulissant.isOn = false
+        eye.isUserInteractionEnabled = true
+        eye.addGestureRecognizer(tapGestureRecognizer)
     }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
+        if eye.image == UIImage(named: "eye_on_icon"){
+            eye.image = UIImage(named: "eye_off")
+            passwordField.isSecureTextEntry = true
+        } else{
+            eye.image = UIImage(named: "eye_on_icon")
+            passwordField.isSecureTextEntry = false
+        }
+    }
+    
+    
+    
 
 
 }
